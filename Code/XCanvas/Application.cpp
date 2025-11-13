8e29cde8a7fbb873937ae1c198263b023c933412
Jake Rieger <contact.jakerieger@gmail.com>
2025-11-13 18:20:06 -0500

    update glfw context version 3.3 -> 4.6

--- Code/XCanvas/Application.cpp
+++ Code/XCanvas/Application.cpp
@@ -67,8 +67,8 @@ namespace X {
     void Application::Initialize() {
         if (!glfwInit()) { throw std::runtime_error("Failed to initialize GLFW"); }
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
